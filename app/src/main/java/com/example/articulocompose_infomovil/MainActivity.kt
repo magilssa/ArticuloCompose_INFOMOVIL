ff2c9b204091367b08857614f47bb65fa7b671a2
magilssa <magilssa@unirioja.es>
2024-02-19 10:34:52 +0100

    Final commit

--- app/src/main/java/com/example/articulocompose_infomovil/MainActivity.kt
+++ app/src/main/java/com/example/articulocompose_infomovil/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,10 +42,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArticuloComposeApp(){
     ArticleCard(
-        title = "",
-        shortDescription = "",
-        longDescription =  "",
-        imagePainter = ""
+        title = "Jetpack Compose tutorial",
+        shortDescription = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+        longDescription =  "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+        imagePainter = painterResource(R.drawable.bg_compose_background)
     )
 }
 
